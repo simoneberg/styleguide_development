f639f2997d82a98ade351e439686690657cbc1d2
Christian Alejandro Zurita <czurita27@gmail.com>
2013-10-11 14:54:28 -0700

    File upload code optimized specially for ie8
    Signed-off-by: Christian Alejandro Zurita <czurita27@gmail.com>

--- demo/js/compass-ie8.js
+++ demo/js/compass-ie8.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-  $('#fileupload_btn_label1, #fileupload_btn_label2, #fileupload_btn_label3, #fileupload_btn_label4, #fileupload_btn_label5, #fileupload_btn_label6, #fileupload_btn_label7, #fileupload_btn_label8').on('click', function(event) {
+
+  $('.fileupload_btn_label').on('click', function(event) {
     event.preventDefault();
     $(this).next().trigger('click');
   });
+
 });
