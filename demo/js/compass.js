8d430725b6f6b1d585b3591c601bf6d10c0310be
Ryan Rauh <ryan.rauh@chaione.com>
2013-10-15 13:16:03 -0500

    #776 adds tooltips to the date markers

--- demo/js/compass.js
+++ demo/js/compass.js
@@ -317,3 +317,19 @@ $('.tabs-slides-container').gettingStarted();
 
 })(jQuery);
 
+$(function() {
+  $( document ).tooltip({
+    position: {
+      my: "center bottom-20",
+      at: "center top",
+      using: function( position, feedback ) {
+        $( this ).css( position );
+        $( "<div>" )
+          .addClass( "arrow" )
+          .addClass( feedback.vertical )
+          .addClass( feedback.horizontal )
+          .appendTo( this );
+    }
+    }
+  });
+});
